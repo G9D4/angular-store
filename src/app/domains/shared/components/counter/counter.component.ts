8ff3e37987835b839e1e271032115671ad0cc98f
Gabriela Díaz <gldt2494@gmail.com>
2024-03-19 12:23:59 -0500

    clase 36

--- src/app/domains/shared/components/counter/counter.component.ts
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, SimpleChanges } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-counter',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.css'
 })
@@ -11,4 +12,18 @@ export class CounterComponent {
   @Input ({required: true}) duration: number = 0;
   @Input ({required: true}) message: string = '';
 
+  // El constructor es lo primero que se ejecuta
+  // Corre antes del render
+  constructor() {
+    // No podemos poner cosas asincronas (no promesas, async, suscribe o items que dependan de tiempo/demora)
+    console.log('constructor');
+    console.log('-'.repeat(10));
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    // Corre despues y durante el render
+    console.log('ngOnChanges');
+    console.log('-'.repeat(10));
+    console.log(changes);
+  }
 }
