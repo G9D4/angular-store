5fc638ba17dc22371cf0115a49cc0de17c884893
Gabriela Díaz <gldt2494@gmail.com>
2024-03-19 13:13:25 -0500

    clase 36.2

--- src/app/domains/shared/components/counter/counter.component.ts
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -21,9 +21,34 @@ export class CounterComponent {
   }
 
   ngOnChanges(changes: SimpleChanges) {
-    // Corre despues y durante el render
+    // Asigna valores por primera vez
+    // Corre despues y durante el render, PERO antes del init
     console.log('ngOnChanges');
     console.log('-'.repeat(10));
     console.log(changes);
+  }
+  
+  ngOnInit() {
+    // Corre luego del render, 1 sola vez
+    // Aqui se pueden llamar promesas, peticiones, fetch a API, cosas async
+    // Para hacer operaciones iniciales con los valores asignados
+    console.log('ngOnInit');
+    console.log('-'.repeat(10));
+    console.log('duration =>', this.duration);
+    console.log('message =>', this.message);
+  }
+
+  ngAfterViewInit() {
+    // Corre luego del render
+    // Corre luego de que los hijos ya fueron renderizados
+    console.log('ngAfterViewInit');
+    console.log('-'.repeat(10));
+  }
+
+  ngOnDestroy() {
+    // Cuando el componente se destruye
+    // Cuando el componente se recupera luego de ser destruido se vuelve a correr todo el ciclo desde el constructor
+    console.log('ngOnDestroy');
+    console.log('-'.repeat(10));
   }
 }
