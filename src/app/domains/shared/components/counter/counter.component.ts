196c839ab0f070943de3fa12dca9294f6ac110ae
Gabriela Díaz <gldt2494@gmail.com>
2024-03-19 11:28:55 -0500

    clase 35

---
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  imports: [],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css'
+})
+export class CounterComponent {
+  @Input ({required: true}) duration: number = 0;
+  @Input ({required: true}) message: string = '';
+
+}
