28ac096d117e72c8a9be71bf0b4b20c2c6351b11
Gabriela Díaz <gldt2494@gmail.com>
2024-03-21 12:52:13 -0500

    clase 45

--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal, Input } from '@angular/core';
+import { Component, signal, Input, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Product } from '../../models/product.model';
 
@@ -11,15 +11,31 @@ import { Product } from '../../models/product.model';
 })
 export class HeaderComponent {
   hideSideMenu = signal(true);
-  deliveryPrice = signal(10);
+  deliveryPrice = signal('10.00');
 
   @Input({required: true}) cart: Product[] = [];
+  subtotal = signal<string>('0');
+  total = signal<string>('0');
 
   toggleSideMenu() {
     this.hideSideMenu.update(prevState => !prevState);
   };
 
-  getTotal() {
+  getSubtotal() {
     return this.cart.reduce((accumulator, product) => accumulator + product.price, 0);
   };
+  
+  getTotal() {
+    return Number(this.deliveryPrice()) + Number(this.subtotal());
+  };
+
+  ngOnChanges(changes: SimpleChanges) {
+    // Se detona si hay un cambio en el input, o sea cart
+     const cart =  changes['cart'];
+     if(cart) {
+      this.subtotal.set(this.getSubtotal().toFixed(2));
+      this.total.set(this.getTotal().toFixed(2));
+     }
+  };
+
 }
