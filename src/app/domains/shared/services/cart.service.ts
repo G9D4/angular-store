f449827267919661bade2f769b164063583c96a7
Gabriela Díaz <gldt2494@gmail.com>
2024-04-04 13:05:36 -0500

    clase 51

--- src/app/domains/shared/services/cart.service.ts
+++ src/app/domains/shared/services/cart.service.ts
@@ -10,14 +10,14 @@ export class CartService {
 
   subtotal= computed(() => {
     const cart = this.cart();
-    return cart.reduce((accumulator, product) => accumulator + product.price, 0).toFixed(2);
+    return cart.reduce((accumulator, product) => accumulator + product.price, 0);
   })
 
-  deliveryPrice = signal('10.00');
+  deliveryPrice = signal(10);
 
   total= computed(() => {
     const totalNumber = Number(this.deliveryPrice()) + Number(this.subtotal());
-    return totalNumber.toFixed(2);
+    return totalNumber;
   })
 
   addToCart(product: Product) {
