98375c83ff5f672f9ad5e9026a98f384f180264c
Gabriela Díaz <gldt2494@gmail.com>
2024-04-08 17:51:43 -0500

    clase 64

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -5,6 +5,8 @@ import { Product } from '@shared/models/product.model';
 import { HeaderComponent } from '@shared/components/header/header.component';
 import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
+import { CategoryService } from '@shared/services/category.service';
+import { Category } from '@shared/models/category.model';
 
 @Component({
   selector: 'app-list',
@@ -15,12 +17,23 @@ import { ProductService } from '@shared/services/product.service';
 })
 export class ListComponent {
   products = signal<Product[]>([]);
+  categories = signal<Category[]>([]);
   private cartService = inject(CartService);
   private productService = inject(ProductService);
+  private categoryService = inject(CategoryService);
   cart = this.cartService.cart;
   total = this.cartService.total;
 
   ngOnInit() {
+    this.getProducts();
+    this.getCategories();
+  }
+
+  addToCart(product: Product) {
+    this.cartService.addToCart(product);
+  }
+
+  private getProducts() {
     this.productService.getProducts()
     .subscribe({
       next: (products) => {
@@ -31,9 +44,17 @@ export class ListComponent {
       }
     })
   }
-
-  addToCart(product: Product) {
-    this.cartService.addToCart(product);
+  
+  private getCategories() {
+    this.categoryService.getAll()
+    .subscribe({
+      next: (data) => {
+        this.categories.set(data);
+      },
+      error: () => {
+        
+      }
+    })
   }
 
 }
